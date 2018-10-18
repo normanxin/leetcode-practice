828732f360c81aececa9b402626e570992c2c6ac
Norman Xin <nxin@SM-MBP15-37.local>
2018-10-18 16:58:45 -0700

    [213] House Robber II

---
+++ 213.house-robber-ii.js
@@ -0,0 +1,63 @@
+/*
+ * [213] House Robber II
+ *
+ * https://leetcode.com/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (34.69%)
+ * Total Accepted:    92.6K
+ * Total Submissions: 266.8K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed. All houses at this place are
+ * arranged in a circle. That means the first house is the neighbor of the last
+ * one. Meanwhile, adjacent houses have security system connected and it will
+ * automatically contact the police if two adjacent houses were broken into on
+ * the same night.
+ * 
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,3,2]
+ * Output: 3
+ * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money
+ * = 2),
+ * because they are adjacent houses.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
+ * 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  return Math.max(helper(nums, 0, nums.length - 2), helper(nums, 1, nums.length - 1));
+};
+
+function helper(nums, from, to) {
+  const dp = [...nums].fill(0);
+
+  for (let i = from; i <= to; i++) {
+    dp[i] = Math.max(dp[i - 2] || 0, dp[i - 3] || 0) + nums[i];
+  }
+
+  return Math.max(...dp, 0);
+}
