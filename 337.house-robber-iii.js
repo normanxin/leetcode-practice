113f9d28a8134e145c9baceab3d2ae718da06a18
Norman Xin <norman.xin@gmail.com>
2018-10-21 15:57:24 -0700

    [337] House Robber III

---
+++ 337.house-robber-iii.js
@@ -0,0 +1,85 @@
+/*
+ * [337] House Robber III
+ *
+ * https://leetcode.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (46.00%)
+ * Total Accepted:    79.8K
+ * Total Submissions: 173.5K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * The thief has found himself a new place for his thievery again. There is
+ * only one entrance to this area, called the "root." Besides the root, each
+ * house has one and only one parent house. After a tour, the smart thief
+ * realized that "all houses in this place forms a binary tree". It will
+ * automatically contact the police if two directly-linked houses were broken
+ * into on the same night.
+ * 
+ * Determine the maximum amount of money the thief can rob tonight without
+ * alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * Output: 7 
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var rob = function(root) {
+  if (!root) {
+    return 0;
+  }
+
+  rob(root.left);
+  rob(root.right);
+
+  let [plan1, plan2] = [root.val, 0];
+
+  if (root.left) {
+    root.left.left && (plan1 += root.left.left.loot);
+    root.left.right && (plan1 += root.left.right.loot);
+    plan2 += root.left.loot;
+  }
+
+  if (root.right) {
+    root.right.left && (plan1 += root.right.left.loot);
+    root.right.right && (plan1 += root.right.right.loot);
+    plan2 += root.right.loot;
+  }
+
+  return (root.loot = Math.max(plan1, plan2));
+};
