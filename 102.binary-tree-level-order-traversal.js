c99f40f0e0b79230ced2a9bb39c1377a488a5e3f
Norman Xin <nxin@SM-MBP15-37.local>
2018-11-30 15:50:18 -0800

    [102] Binary Tree Level Order Traversal

---
+++ 102.binary-tree-level-order-traversal.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=102 lang=javascript
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (45.63%)
+ * Total Accepted:    302.5K
+ * Total Submissions: 663.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  const res = [];
+
+  if (!root) {
+    return res;
+  }
+
+  let queue = [root];
+
+  while (queue.length) {
+    const next = [];
+
+    for (let node of queue) {
+      node.left && next.push(node.left);
+      node.right && next.push(node.right);
+    }
+
+    res.push(queue.map(node => node.val));
+    queue = next;
+  }
+
+  return res;
+};
