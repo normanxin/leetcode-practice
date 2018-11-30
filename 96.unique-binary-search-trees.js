1d4604e12e216ba4d8d7718b4b589c567fabb4b6
Norman Xin <norman.xin@gmail.com>
2018-11-30 08:52:03 -0800

    [96] Unique Binary Search Trees

---
+++ 96.unique-binary-search-trees.js
@@ -0,0 +1,47 @@
+/*
+ * [96] Unique Binary Search Trees
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (43.96%)
+ * Total Accepted:    172.9K
+ * Total Submissions: 393.2K
+ * Testcase Example:  '3'
+ *
+ * Given n, how many structurally unique BST's (binary search trees) that store
+ * values 1 ... n?
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output: 5
+ * Explanation:
+ * Given n = 3, there are a total of 5 unique BST's:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  const dp = Array(n + 1).fill(0);
+
+  dp[0] = 1;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      dp[i] += dp[j] * dp[i - 1 - j];
+    }
+  }
+
+  return dp[n];
+};
