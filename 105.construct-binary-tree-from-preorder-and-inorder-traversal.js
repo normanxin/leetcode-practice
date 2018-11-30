f9c424c3c588f27e47153e0b837e6c714ccd504a
Norman Xin <nxin@SM-MBP15-37.local>
2018-11-29 19:31:56 -0800

    [105] Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=105 lang=javascript
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (37.84%)
+ * Total Accepted:    180.6K
+ * Total Submissions: 477.4K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ * 
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * 
+ * For example, given
+ * 
+ * 
+ * preorder = [3,9,20,15,7]
+ * inorder = [9,3,15,20,7]
+ * 
+ * Return the following binary tree:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+function TreeNode(val) {
+       this.val = val;
+       this.left = this.right = null;
+   }
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+  if (!preorder.length) {
+    return null;
+  }
+
+  const root = new TreeNode(preorder[0]);
+  const idx = inorder.indexOf(root.val);
+
+  root.left = buildTree(preorder.slice(1, 1 + idx), inorder.slice(0, idx));
+  root.right = buildTree(preorder.slice(1 + idx), inorder.slice(1 + idx));
+
+  return root;
+};
