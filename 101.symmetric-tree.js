df7a3d8c0ad6ed41a714c8ada51794bd8fcef1b3
Norman Xin <nxin@SM-MBP15-37.local>
2018-11-29 19:54:38 -0800

    [101] Symmetric Tree

---
+++ 101.symmetric-tree.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (41.85%)
+ * Total Accepted:    320.6K
+ * Total Submissions: 766.2K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3]  is not:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) {
+    return true;
+  }
+
+  return helper(root.left, root.right);
+};
+
+function helper(p, q) {
+  if (!p && !q) {
+    return true;
+  }
+
+  if (!p || !q || p.val !== q.val) {
+    return false;
+  }
+
+  return helper(p.left, q.right) && helper(p.right, q.left);
+}
