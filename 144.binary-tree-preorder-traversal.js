e1959aa4020ea14a065e74edecad6ab6606e2dd2
Norman Xin <norman.xin@gmail.com>
2018-12-01 08:04:46 -0800

    [144] Binary Tree Preorder Traversal

---
+++ 144.binary-tree-preorder-traversal.js
@@ -0,0 +1,57 @@
+/*
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (49.20%)
+ * Total Accepted:    280K
+ * Total Submissions: 569.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,2,3]
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  const res = [];
+  
+  if (!root) {
+    return res;
+  }
+
+  for (const stack = [root]; stack.length;) {
+    const cur = stack.pop();
+
+    res.push(cur.val);
+    cur.right && stack.push(cur.right);
+    cur.left && stack.push(cur.left);
+  }
+
+  return res;
+};
