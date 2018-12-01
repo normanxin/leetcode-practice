8fe4be7874232669d643286e0b3c1e26cf770dcb
Norman Xin <nxin@SM-MBP15-37.local>
2018-11-30 16:30:03 -0800

    [106] Construct Binary Tree from Inorder and Postorder Traversal

---
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=106 lang=javascript
+ *
+ * [106] Construct Binary Tree from Inorder and Postorder Traversal
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (36.67%)
+ * Total Accepted:    130.5K
+ * Total Submissions: 355.9K
+ * Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+ *
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ * 
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * 
+ * For example, given
+ * 
+ * 
+ * inorder = [9,3,15,20,7]
+ * postorder = [9,15,7,20,3]
+ * 
+ * Return the following binary tree:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+  if (!inorder.length) {
+    return null;
+  }
+
+  const root = new TreeNode(postorder[postorder.length - 1]);
+  const idx = inorder.indexOf(root.val);
+
+  root.left = buildTree(inorder.slice(0, idx), postorder.slice(0, idx));
+  root.right = buildTree(inorder.slice(idx + 1), postorder.slice(idx, postorder.length - 1));
+
+  return root;
+};
