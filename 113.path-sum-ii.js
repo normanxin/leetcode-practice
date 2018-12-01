622b2b01d3cb45cdc17c820fe5a5b4aba36c417c
Norman Xin <nxin@SM-MBP15-37.local>
2018-11-30 17:17:19 -0800

    [113] Path Sum II

---
+++ 113.path-sum-ii.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=113 lang=javascript
+ *
+ * [113] Path Sum II
+ *
+ * https://leetcode.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (38.26%)
+ * Total Accepted:    197.6K
+ * Total Submissions: 516.4K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * Given a binary tree and a sum, find all root-to-leaf paths where each path's
+ * sum equals the given sum.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given the below binary tree and sum = 22,
+ * 
+ * 
+ * ⁠     5
+ * ⁠    / \
+ * ⁠   4   8
+ * ⁠  /   / \
+ * ⁠ 11  13  4
+ * ⁠/  \    / \
+ * 7    2  5   1
+ * 
+ * 
+ * Return:
+ * 
+ * 
+ * [
+ * ⁠  [5,4,11,2],
+ * ⁠  [5,8,4,5]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function(root, sum, res = [], path = []) {
+  if (!root) {
+    return res;
+  }
+
+  path.push(root.val);
+
+  if (!root.left && !root.right && sum === root.val) {
+    res.push([...path]);
+  }
+
+  const next = sum - root.val;
+
+  pathSum(root.left, next, res, path);
+  pathSum(root.right, next, res, path);
+  path.pop();
+
+  return res;
+};
