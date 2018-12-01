8ea55f3455bd7a1561a0946aebe0c2d126b12394
Norman Xin <norman.xin@gmail.com>
2018-12-01 07:53:30 -0800

    [129] Sum Root to Leaf Numbers

---
+++ 129.sum-root-to-leaf-numbers.js
@@ -0,0 +1,85 @@
+/*
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (40.24%)
+ * Total Accepted:    160.1K
+ * Total Submissions: 397.9K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+  let res = 0;
+
+  helper(root, []);
+
+  return res;
+
+  function helper(root, path) {
+    if (!root) {
+      return;
+    }
+
+    path.push(root.val);
+
+    if (!root.left && !root.right) {
+      res += +path.join('');
+    }
+
+    helper(root.left, path);
+    helper(root.right, path);
+    path.pop();
+  }
+};
