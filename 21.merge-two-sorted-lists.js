af4748dcc70db17fc549d12bf4b023e51bdae251
Norman Xin <norman.xin@gmail.com>
2018-12-08 14:05:55 -0800

    [21] Merge Two Sorted Lists

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,57 @@
+/*
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (44.29%)
+ * Total Accepted:    452.7K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  const dummy = new ListNode();
+  let cur = dummy;
+
+  for (;l1 && l2; cur = cur.next) {
+    if (l1.val < l2.val) {
+      cur.next = l1;
+      l1 = l1.next;
+    } else {
+      cur.next = l2;
+      l2 = l2.next;
+    }
+  }
+
+  for (; l1; l1 = l1.next, cur = cur.next) {
+    cur.next = l1;
+  }
+
+  for (; l2; l2 = l2.next, cur = cur.next) {
+    cur.next = l2;
+  }
+
+  return dummy.next;
+};
