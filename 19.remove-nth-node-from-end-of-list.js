7a5951802419a9a5719922ec1f685b6aed2d9bcd
Norman Xin <nxin@SM-MBP15-37.local>
2018-12-10 11:17:09 -0800

    [19] Remove Nth Node From End of List

---
+++ 19.remove-nth-node-from-end-of-list.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=19 lang=javascript
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (33.66%)
+ * Total Accepted:    320.3K
+ * Total Submissions: 951.6K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, remove the n-th node from the end of list and return
+ * its head.
+ * 
+ * Example:
+ * 
+ * 
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * 
+ * After removing the second node from the end, the linked list becomes
+ * 1->2->3->5.
+ * 
+ * 
+ * Note:
+ * 
+ * Given n will always be valid.
+ * 
+ * Follow up:
+ * 
+ * Could you do this in one pass?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const dummy = new ListNode();
+  let lo = dummy; 
+  let hi = dummy;
+
+  dummy.next = head;
+
+  for (let i = 0; i <= n; i++, hi = hi.next) {
+    if (!hi) {
+      return dummy.next;
+    }
+  }
+
+  while (hi) {
+    lo = lo.next;
+    hi = hi.next;
+  }
+
+  lo.next = lo.next.next;
+
+  return dummy.next;
+};
