ade3f71729ef40d97f96f0c405f87fc96e97669a
Norman Xin <nxin@SM-MBP15-37.local>
2018-12-11 13:09:32 -0800

    [160] Intersection of Two Linked Lists

---
+++ 160.intersection-of-two-linked-lists.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=160 lang=javascript
+ *
+ * [160] Intersection of Two Linked Lists
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (30.86%)
+ * Total Accepted:    248.5K
+ * Total Submissions: 805.1K
+ * Testcase Example:  'No intersection: []\n[]'
+ *
+ * Write a program to find the node at which the intersection of two singly
+ * linked lists begins.
+ * 
+ * For example, the following two linked lists: 
+ * 
+ * A:          a1 → a2
+ * ⁠                  ↘
+ * ⁠                    c1 → c2 → c3
+ * ⁠                  ↗            
+ * B:     b1 → b2 → b3
+ * 
+ * begin to intersect at node c1.
+ * 
+ * Notes:
+ * 
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function
+ * returns. 
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * 
+ * 
+ * 
+ * Credits:Special thanks to @stellari for adding this problem and creating all
+ * test cases.
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  let curA = headA;
+  let curB = headB;
+
+  while (curA && curB) {
+    curA = curA.next;
+    curB = curB.next;
+  }
+
+  if (!curA) {
+    for (; curB; curB = curB.next) {
+      headB = headB.next;
+    }
+  }
+
+  if (!curB) {
+    for (; curA; curA = curA.next) {
+      headA = headA.next;
+    }
+  }
+
+  while (headA !== headB) {
+    headA = headA.next;
+    headB = headB.next;
+  }
+
+  return headA;
+};
