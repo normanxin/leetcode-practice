64c7b75ff569205ed421410c3629d4e9c42b4107
Norman Xin <nxin@SM-MBP15-37.local>
2018-12-14 12:51:27 -0800

    [535] Encode and Decode TinyURL

---
+++ 535.encode-and-decode-tinyurl.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=535 lang=javascript
+ *
+ * [535] Encode and Decode TinyURL
+ *
+ * https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+ *
+ * algorithms
+ * Medium (74.95%)
+ * Total Accepted:    58.3K
+ * Total Submissions: 77.8K
+ * Testcase Example:  '"https://leetcode.com/problems/design-tinyurl"'
+ *
+ * Note: This is a companion problem to the System Design problem: Design
+ * TinyURL.
+ * 
+ * TinyURL is a URL shortening service where you enter a URL such as
+ * https://leetcode.com/problems/design-tinyurl and it returns a short URL such
+ * as http://tinyurl.com/4e9iAk.
+ * 
+ * Design the encode and decode methods for the TinyURL service. There is no
+ * restriction on how your encode/decode algorithm should work. You just need
+ * to ensure that a URL can be encoded to a tiny URL and the tiny URL can be
+ * decoded to the original URL.
+ * 
+ */
+
+ const cache = [];
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+  const res = cache.length;
+
+  cache.push(longUrl);
+
+  return res;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+  return cache[shortUrl];
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
