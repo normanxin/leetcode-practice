00a20553a184c1874e03f1e4823cad69f6e705f6
Norman Xin <norman.xin@gmail.com>
2018-12-18 08:47:04 -0800

    [150] Evaluate Reverse Polish Notation

---
+++ 150.evaluate-reverse-polish-notation.js
@@ -0,0 +1,90 @@
+/*
+ * [150] Evaluate Reverse Polish Notation
+ *
+ * https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+ *
+ * algorithms
+ * Medium (30.61%)
+ * Total Accepted:    140.4K
+ * Total Submissions: 458.7K
+ * Testcase Example:  '["2","1","+","3","*"]'
+ *
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * 
+ * Valid operators are +, -, *, /. Each operand may be an integer or another
+ * expression.
+ * 
+ * Note:
+ * 
+ * 
+ * Division between two integers should truncate toward zero.
+ * The given RPN expression is always valid. That means the expression would
+ * always evaluate to a result and there won't be any divide by zero
+ * operation.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["2", "1", "+", "3", "*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: ["4", "13", "5", "/", "+"]
+ * Output: 6
+ * Explanation: (4 + (13 / 5)) = 6
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+ * Output: 22
+ * Explanation: 
+ * ⁠ ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ * 
+ * 
+ */
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+  const stack = [];
+
+  for (let token of tokens) {
+    if (isNaN(token)) {
+      const op2 = stack.pop();
+      const op1 = stack.pop();
+
+      switch (token) {
+        case '+':
+          stack.push(op1 + op2);
+          break;
+        case '-':
+          stack.push(op1 - op2);
+          break;
+        case '*':
+          stack.push(op1 * op2);
+          break;
+        case '/':
+          stack.push(~~(op1 / op2));
+          break;
+      }
+    } else {
+      stack.push(+token);
+    }
+  }
+
+  return stack.pop();
+};
