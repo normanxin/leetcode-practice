15c7bb665a8939facfb33f5f76d209852bdf4387
Norman Xin <nxin@SM-MBP15-37.local>
2018-12-20 15:53:33 -0800

    [136] Single Number

---
+++ 136.single-number.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=136 lang=javascript
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (57.91%)
+ * Total Accepted:    383.2K
+ * Total Submissions: 661.7K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,1]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let res = 0;
+
+  for (let num of nums) {
+    res ^= num;
+  }
+
+  return res;
+};
