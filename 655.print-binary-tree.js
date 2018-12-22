cedb79f2ac916d8827186bafae8d102f6a54c575
Norman Xin <norman.xin@gmail.com>
2018-12-21 21:56:40 -0800

    [655] Print Binary Tree

---
+++ 655.print-binary-tree.js
@@ -0,0 +1,117 @@
+/*
+ * [655] Print Binary Tree
+ *
+ * https://leetcode.com/problems/print-binary-tree/description/
+ *
+ * algorithms
+ * Medium (49.86%)
+ * Total Accepted:    15.2K
+ * Total Submissions: 30.5K
+ * Testcase Example:  '[1,2]'
+ *
+ * Print a binary tree in an m*n 2D string array following these rules: 
+ * 
+ * 
+ * The row number m should be equal to the height of the given binary tree.
+ * The column number n should always be an odd number.
+ * The root node's value (in string format) should be put in the exactly middle
+ * of the first row it can be put. The column and the row where the root node
+ * belongs will separate the rest space into two parts (left-bottom part and
+ * right-bottom part). You should print the left subtree in the left-bottom
+ * part and print the right subtree in the right-bottom part. The left-bottom
+ * part and the right-bottom part should have the same size. Even if one
+ * subtree is none while the other is not, you don't need to print anything for
+ * the none subtree but still need to leave the space as large as that for the
+ * other subtree. However, if two subtrees are none, then you don't need to
+ * leave space for both of them. 
+ * Each unused space should contain an empty string "".
+ * Print the subtrees following the same rules.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * ⁠    1
+ * ⁠   /
+ * ⁠  2
+ * Output:
+ * [["", "1", ""],
+ * ⁠["2", "", ""]]
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * ⁠    1
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \
+ * ⁠    4
+ * Output:
+ * [["", "", "", "1", "", "", ""],
+ * ⁠["", "2", "", "", "", "3", ""],
+ * ⁠["", "", "4", "", "", "", ""]]
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input:
+ * ⁠     1
+ * ⁠    / \
+ * ⁠   2   5
+ * ⁠  / 
+ * ⁠ 3 
+ * ⁠/ 
+ * 4 
+ * Output:
+ * 
+ * [["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
+ * ⁠["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
+ * ⁠["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
+ * ⁠["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
+ * 
+ * 
+ * 
+ * Note:
+ * The height of binary tree is in the range of [1, 10].
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[][]}
+ */
+var printTree = function(root) {
+  const d = depth(root);
+  const res = Array(d).fill().map(_ => Array(2 ** d - 1).fill(''));
+
+  return helper(root, res, 0, res[0].length - 1);
+}
+
+function depth(root) {
+  return root ? Math.max(depth(root.left), depth(root.right)) + 1 : 0;
+}
+
+function helper(root, res, lo, hi, level = 0) {
+  if (!root) {
+    return res;
+  }
+
+  const mid = lo + Math.floor((hi - lo) / 2);
+
+  res[level][mid] = root.val.toString();
+
+  helper(root.left, res, lo, mid - 1, level + 1);
+  helper(root.right, res, mid + 1, hi, level + 1);
+
+  return res;
+}
