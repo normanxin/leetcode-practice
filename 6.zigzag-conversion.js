dcb117aec9d5cac015bf0e09f4e82c7e25de4a1e
Norman Xin <norman.xin@gmail.com>
2018-12-29 11:52:31 -0800

    [6] ZigZag Conversion

---
+++ 6.zigzag-conversion.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=6 lang=javascript
+ *
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (29.74%)
+ * Total Accepted:    266.2K
+ * Total Submissions: 894.3K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ * 
+ * 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * 
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ * 
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * 
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 
+ */
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
+
+  const res = Array(numRows).fill('');
+
+  for (let i = 0, x = 0, dx = -1; s[i]; i++, x += dx) {
+    res[x] += s[i];
+
+    if (x === 0 || x === numRows - 1) {
+      dx *= -1;
+    }
+  }
+
+  return res.join('');
+};
