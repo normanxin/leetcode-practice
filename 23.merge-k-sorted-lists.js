4e07c80c36c8f84662327495f9ed38a51b0dc94e
Norman Xin <norman.xin@gmail.com>
2019-01-20 10:09:03 -0800

    [23] Merge k Sorted Lists

---
+++ 23.merge-k-sorted-lists.js
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=23 lang=javascript
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (32.13%)
+ * Total Accepted:    320.1K
+ * Total Submissions: 995.9K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+  return lists.reduce((res, list) => merge(res, list), null);
+};
+
+function merge(l1, l2) {
+  const dummy = new ListNode();
+  let cur = dummy;
+
+  for (; l1 && l2; cur = cur.next) {
+    if (l1.val < l2.val) {
+      cur.next = l1;
+      l1 = l1.next;
+    } else {
+      cur.next = l2;
+      l2 = l2.next;
+    }
+  }
+
+  l1 && (cur.next = l1);
+  l2 && (cur.next = l2);
+
+  return dummy.next;
+}
