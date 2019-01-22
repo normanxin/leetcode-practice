1a5f0c63d992c2bd8f64405fc2955fe1fbcb5f9a
Norman Xin <nxin@SM-MBP15-37.local>
2019-01-22 10:11:18 -0800

    [184] Department Highest Salary

---
+++ 184.department-highest-salary.sql
@@ -0,0 +1,55 @@
+--
+-- @lc app=leetcode id=184 lang=mysql
+--
+-- [184] Department Highest Salary
+--
+-- https://leetcode.com/problems/department-highest-salary/description/
+--
+-- database
+-- Medium (26.22%)
+-- Total Accepted:    51.2K
+-- Total Submissions: 195.4K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Name", "Salary", "DepartmentId"], "Department": ["Id", "Name"]}, "rows": {"Employee": [[1, "Joe", 70000, 1], [2, "Henry", 80000, 2], [3, "Sam", 60000, 2], [4, "Max", 90000, 1]], "Department": [[1, "IT"], [2, "Sales"]]}}'
+--
+-- The Employee table holds all employees. Every employee has an Id, a salary,
+-- and there is also a column for the department Id.
+-- 
+-- 
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Henry | 80000  | 2            |
+-- | 3  | Sam   | 60000  | 2            |
+-- | 4  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- 
+-- 
+-- The Department table holds all departments of the company.
+-- 
+-- 
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- 
+-- 
+-- Write a SQL query to find employees who have the highest salary in each of
+-- the departments. For the above tables, Max has the highest salary in the IT
+-- department and Henry has the highest salary in the Sales department.
+-- 
+-- 
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT d.Name AS Department, e.Name AS Employee, e.Salary FROM Employee e, Department d
+WHERE e.DepartmentId = d.Id AND e.Salary = (SELECT MAX(Salary) FROM Employee e2 WHERE e2.DepartmentId = d.Id);
