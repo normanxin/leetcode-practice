eeab07e582cc336035fbd665e7b1dd088dd6f5e0
Norman Xin <nxin@SM-MBP15-37.local>
2019-01-24 10:31:44 -0800

    [197] Rising Temperature

---
+++ 197.rising-temperature.sql
@@ -0,0 +1,43 @@
+--
+-- @lc app=leetcode id=197 lang=mysql
+--
+-- [197] Rising Temperature
+--
+-- https://leetcode.com/problems/rising-temperature/description/
+--
+-- database
+-- Easy (33.14%)
+-- Total Accepted:    64.4K
+-- Total Submissions: 194.5K
+-- Testcase Example:  '{"headers": {"Weather": ["Id", "RecordDate", "Temperature"]}, "rows": {"Weather": [[1, "2015-01-01", 10], [2, "2015-01-02", 25], [3, "2015-01-03", 20], [4, "2015-01-04", 30]]}}'
+--
+-- Given a Weather table, write a SQL query to find all dates' Ids with higher
+-- temperature compared to its previous (yesterday's) dates.
+-- 
+-- 
+-- +---------+------------------+------------------+
+-- | Id(INT) | RecordDate(DATE) | Temperature(INT) |
+-- +---------+------------------+------------------+
+-- |       1 |       2015-01-01 |               10 |
+-- |       2 |       2015-01-02 |               25 |
+-- |       3 |       2015-01-03 |               20 |
+-- |       4 |       2015-01-04 |               30 |
+-- +---------+------------------+------------------+
+-- 
+-- 
+-- For example, return the following Ids for the above Weather table:
+-- 
+-- 
+-- +----+
+-- | Id |
+-- +----+
+-- |  2 |
+-- |  4 |
+-- +----+
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT w1.Id FROM Weather w1, Weather w2
+WHERE w1.Temperature > w2.Temperature
+AND DATEDIFF(w1.RecordDate, w2.RecordDate) = 1;
