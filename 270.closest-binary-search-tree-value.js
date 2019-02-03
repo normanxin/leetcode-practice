4e83d154908e50f5a551d0def948a5fe7753cdde
Norman Xin <norman.xin@gmail.com>
2019-02-02 17:35:31 -0800

    [270] Closest Binary Search Tree Value

---
+++ 270.closest-binary-search-tree-value.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=270 lang=javascript
+ *
+ * [270] Closest Binary Search Tree Value
+ *
+ * https://leetcode.com/problems/closest-binary-search-tree-value/description/
+ *
+ * algorithms
+ * Easy (42.78%)
+ * Total Accepted:    76K
+ * Total Submissions: 177.7K
+ * Testcase Example:  '[4,2,5,1,3]\n3.714286'
+ *
+ * Given a non-empty binary search tree and a target value, find the value in
+ * the BST that is closest to the target.
+ * 
+ * Note:
+ * 
+ * 
+ * Given target value is a floating point.
+ * You are guaranteed to have only one unique value in the BST that is closest
+ * to the target.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: root = [4,2,5,1,3], target = 3.714286
+ * 
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \
+ * 1   3
+ * 
+ * Output: 4
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, target) {
+  if (!root) {
+    return Infinity;
+  }
+
+  if (root.val === target) {
+    return root.val;
+  }
+
+  const candidate = closestValue(target < root.val ? root.left : root.right, target); 
+
+  return Math.abs(root.val - target) < Math.abs(candidate - target)
+      ? root.val
+      : candidate; 
+};
