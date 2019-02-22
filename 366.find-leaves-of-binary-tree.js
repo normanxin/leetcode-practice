6a1a324e997ec916735805f6961b2945264913c2
Norman Xin <nxin@SM-MBP15-37.local>
2019-02-22 10:19:29 -0800

    [366] Find Leaves of Binary Tree

---
+++ 366.find-leaves-of-binary-tree.js
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=366 lang=javascript
+ *
+ * [366] Find Leaves of Binary Tree
+ *
+ * https://leetcode.com/problems/find-leaves-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (63.91%)
+ * Total Accepted:    37.9K
+ * Total Submissions: 59.3K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a binary tree, collect a tree's nodes as if you were doing this:
+ * Collect and remove all leaves, repeat until the tree is empty.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * 
+ * 1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * Output: [[4,5,3],[2],[1]]
+ * 
+ * 
+ * 
+ * 
+ * Explanation:
+ * 
+ * 1. Removing the leaves [4,5,3] would result in this tree:
+ * 
+ * 
+ * ⁠         1
+ * ⁠        / 
+ * ⁠       2          
+ * 
+ * 
+ * 
+ * 
+ * 2. Now removing the leaf [2] would result in this tree:
+ * 
+ * 
+ * ⁠         1          
+ * 
+ * 
+ * 
+ * 
+ * 3. Now removing the leaf [1] would result in the empty tree:
+ * 
+ * 
+ * ⁠         []         
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var findLeaves = function(root, res = []) {
+  if (!root) {
+    return res;
+  }
+
+  findLeaves(root.left, res);
+  findLeaves(root.right, res);
+
+  root.lv = Math.max(
+      root.left ? root.left.lv : -1, 
+      root.right ? root.right.lv : -1) + 1;
+  res[root.lv] = res[root.lv] || [];
+  res[root.lv].push(root.val);
+
+  return res;
+};
