8865c9e2ad5d2039fd149d19e1ae80471785b492
Norman Xin <norman.xin@gmail.com>
2019-03-10 21:22:09 -0700

    [344] Reverse String

---
+++ 344.reverse-string.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (62.81%)
+ * Total Accepted:    381.4K
+ * Total Submissions: 607.1K
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters char[].
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ * 
+ * You may assume all the characters consist of printable ascii
+ * characters.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  for (let l = 0, r = s.length - 1; l < r; l++, r--) {
+    [s[l], s[r]] = [s[r], s[l]];
+  }
+};
