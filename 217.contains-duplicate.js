c72b249c8ce6cdfffcd1ab8010299405cc84d1ae
Norman Xin <norman.xin@gmail.com>
2019-04-01 20:43:53 -0700

    [217] Contains Duplicate

---
+++ 217.contains-duplicate.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=217 lang=javascript
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (51.24%)
+ * Total Accepted:    314.9K
+ * Total Submissions: 614.6K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ * 
+ * Your function should return true if any value appears at least twice in the
+ * array, and it should return false if every element is distinct.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4]
+ * Output: false
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  return nums.some((num, i) => nums.indexOf(num) !== i);
+};
