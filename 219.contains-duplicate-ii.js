46fc70956fdd2bbf093eaf265629813f0cfb5cda
Norman Xin <norman.xin@gmail.com>
2019-04-02 20:36:30 -0700

    [219] Contains Duplicate II

---
+++ 219.contains-duplicate-ii.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=219 lang=javascript
+ *
+ * [219] Contains Duplicate II
+ *
+ * https://leetcode.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (34.92%)
+ * Total Accepted:    188.8K
+ * Total Submissions: 540.6K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * Given an array of integers and an integer k, find out whether there are two
+ * distinct indices i and j in the array such that nums[i] = nums[j] and the
+ * absolute difference between i and j is at most k.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  for (let i = 0, map = new Map(); i < nums.length; i++) {
+    if (map.has(nums[i]) && Math.abs(i - map.get(nums[i]) <= k)) {
+      return true;
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return false;
+};
