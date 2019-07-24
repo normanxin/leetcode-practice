ac0fe6ca5aad3c877c23a8f5f20e334d39d79420
Norman Xin <nxin@SM-MBP15-37.local>
2019-07-24 16:24:52 -0700

    [461] Hamming Distance

---
+++ 461.hamming-distance.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=461 lang=javascript
+ *
+ * [461] Hamming Distance
+ *
+ * https://leetcode.com/problems/hamming-distance/description/
+ *
+ * algorithms
+ * Easy (70.53%)
+ * Total Accepted:    249.4K
+ * Total Submissions: 353.6K
+ * Testcase Example:  '1\n4'
+ *
+ * The Hamming distance between two integers is the number of positions at
+ * which the corresponding bits are different.
+ * 
+ * Given two integers x and y, calculate the Hamming distance.
+ * 
+ * Note:
+ * 0 ≤ x, y < 231.
+ * 
+ * 
+ * Example:
+ * 
+ * Input: x = 1, y = 4
+ * 
+ * Output: 2
+ * 
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ * ⁠      ↑   ↑
+ * 
+ * The above arrows point to positions where the corresponding bits are
+ * different.
+ * 
+ * 
+ */
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const hammingDistance = (x, y) => {
+  let res = 0;
+
+  for (let i = x ^ y; i > 0; i >>= 1) {
+    res += i & 1;
+  }
+
+  return res;
+};
