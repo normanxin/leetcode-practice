e689f619163afeb8ac52c311404c488b92b89377
Norman Xin <norman.xin@gmail.com>
2019-07-25 22:19:16 -0700

    [543] Diameter of Binary Tree

---
+++ 543.diameter-of-binary-tree.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=543 lang=javascript
+ *
+ * [543] Diameter of Binary Tree
+ *
+ * https://leetcode.com/problems/diameter-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (47.11%)
+ * Total Accepted:    143.3K
+ * Total Submissions: 304.2K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 
+ * Given a binary tree, you need to compute the length of the diameter of the
+ * tree. The diameter of a binary tree is the length of the longest path
+ * between any two nodes in a tree. This path may or may not pass through the
+ * root.
+ * 
+ * 
+ * 
+ * Example:
+ * Given a binary tree 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * 
+ * 
+ * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ * 
+ * 
+ * Note:
+ * The length of path between two nodes is represented by the number of edges
+ * between them.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const diameterOfBinaryTree = root => {
+  if (!root) {
+    return 0;
+  }
+
+  const lDiameter = diameterOfBinaryTree(root.left);
+  const rDiameter = diameterOfBinaryTree(root.right);
+  const lHeight = root.left ? root.left.height : 0;
+  const rHeight = root.right ? root.right.height : 0;
+
+  root.height = 1 + Math.max(lHeight, rHeight);
+
+  return Math.max(lDiameter, rDiameter, lHeight + rHeight);
+};
