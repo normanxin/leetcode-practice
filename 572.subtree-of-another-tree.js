7a81660ad50a621b102c75fd6b44204714b117a7
Norman Xin <norman.xin@gmail.com>
2019-07-27 12:18:12 -0700

    [572] Subtree of Another Tree

---
+++ 572.subtree-of-another-tree.js
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=572 lang=javascript
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (41.93%)
+ * Total Accepted:    107.8K
+ * Total Submissions: 257.1K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 
+ * Given two non-empty binary trees s and t, check whether tree t has exactly
+ * the same structure and node values with a subtree of s. A subtree of s is a
+ * tree consists of a node in s and all of this node's descendants. The tree s
+ * could also be considered as a subtree of itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given tree s:
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Given tree t:
+ * 
+ * ⁠  4 
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Return true, because t has the same structure and node values with a subtree
+ * of s.
+ * 
+ * 
+ * Example 2:
+ * 
+ * Given tree s:
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ * 
+ * Given tree t:
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Return false.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+const isSubtree = (s, t) => {
+  if (!t) {
+    return true;
+  }
+
+  if (!s) {
+    return false;
+  }
+
+  let res = isSameTree(s, t);
+
+  if (res) {
+    return res;
+  }
+
+  return isSubtree(s.left, t) || isSubtree(s.right, t);
+};
+
+function isSameTree(s, t) {
+  if (!s && !t) {
+    return true;
+  }
+
+  if (!s || !t || s.val !== t.val) {
+    return false;
+  }
+
+  return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+}
